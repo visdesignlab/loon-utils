27c07b5c614f422212acdf1f87a09ed23a6f2a5c
Luke Schreiber <131496683+Luke-Schreiber@users.noreply.github.com>
2025-10-28 15:16:08 -0600

    Bump version print hello

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'loon-utils=loon_utils.main:main',
+            'print-hello=loon_utils.main:hello',
         ],
     },
 )
